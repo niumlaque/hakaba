0cc29fd85b454771da9e6155dc29aaea053f2c5e
Niumlaque <niumlaque@gmail.com>
2019-10-08 18:41:19 +0900

    9cc ~Step3

--- golang/9cc/9cc.go
+++ golang/9cc/9cc.go
@@ -1,10 +1,106 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
 )
+
+type TokenKind int
+
+const (
+	Reserved TokenKind = iota
+	Num
+	EOF
+)
+
+type Token struct {
+	kind TokenKind
+	next *Token
+	val  int
+	str  []rune
+}
+
+func (tk *Token) String() string {
+	var nextRune rune
+	if len(tk.str) > 0 {
+		nextRune = tk.str[0]
+	}
+
+	return fmt.Sprintf("Kind=%v, NextRune=%v, Val=%v", tk.kind, string(nextRune), tk.val)
+}
+
+func NewToken(kind TokenKind, currentToken *Token, str []rune) *Token {
+	v := &Token{}
+	v.kind = kind
+	v.str = str
+	currentToken.next = v
+	return v
+}
+
+var token *Token
+
+func consume(op rune) bool {
+	if token.kind != Reserved || token.str[0] != op {
+		return false
+	}
+
+	token = token.next
+	return true
+}
+
+func expect(op rune) {
+	if token.kind != Reserved || token.str[0] != op {
+		panic(fmt.Errorf("Unexpected operator '%v'", op))
+	}
+
+	token = token.next
+}
+
+func expectNumber() int {
+	if token.kind != Num {
+		panic(errors.New("Not a anumber"))
+	}
+
+	v := token.val
+	token = token.next
+	return v
+}
+
+func atEOF() bool {
+	return token.kind == EOF
+}
+
+func tokenize(p []rune) *Token {
+	var head Token
+	head.next = nil
+	currentToken := &head
+
+	for len(p) > 0 {
+		if p[0] == ' ' {
+			p = p[1:]
+			continue
+		}
+
+		if p[0] == '+' || p[0] == '-' {
+			currentToken = NewToken(Reserved, currentToken, p)
+			p = p[1:]
+			continue
+		}
+
+		if '0' <= p[0] && p[0] <= '9' {
+			currentToken = NewToken(Num, currentToken, p)
+			currentToken.val = strtol(p, &p, 10)
+			continue
+		}
+
+		panic(fmt.Errorf("Failed to tokenize text: '%v'", p[0]))
+	}
+
+	NewToken(EOF, currentToken, p)
+	return head.next
+}
 
 func strtol(nptr []rune, endptr *[]rune, base int) int {
 	var p rune
@@ -37,32 +133,38 @@ func strtol(nptr []rune, endptr *[]rune, base int) int {
 	return 0
 }
 
+func dumpToken(v *Token) {
+	cur := v
+	for i := 0; cur != nil; i++ {
+		fmt.Fprintf(os.Stderr, "[%v] %s\n", i+1, cur)
+		cur = cur.next
+	}
+}
+
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Fprintf(os.Stderr, "Invalid parameter\n")
 		os.Exit(1)
 	}
 
-	p := []rune(os.Args[1])
+	token = tokenize([]rune(os.Args[1]))
 	fmt.Fprintf(os.Stdout, ".intel_syntax noprefix\n")
 	fmt.Fprintf(os.Stdout, ".global main\n")
 	fmt.Fprintf(os.Stdout, "main:\n")
-	fmt.Fprintf(os.Stdout, "  mov rax, %d\n", strtol(p, &p, 10))
+	fmt.Fprintf(os.Stdout, "  mov rax, %v\n", expectNumber())
 
-	for len(p) > 0 {
-		if p[0] == '+' {
-			p = p[1:]
-			fmt.Fprintf(os.Stdout, "  add rax, %v\n", strtol(p, &p, 10))
-			continue
-		}
-		if p[0] == '-' {
-			p = p[1:]
-			fmt.Fprintf(os.Stdout, "  sub rax, %v\n", strtol(p, &p, 10))
+	for !atEOF() {
+		if consume('+') {
+			fmt.Fprintf(os.Stdout, "  add rax, %v\n", expectNumber())
 			continue
 		}
 
-		fmt.Fprintf(os.Stderr, "Unecpected character: '%v'", p[0])
-		os.Exit(1)
+		if consume('-') {
+			fmt.Fprintf(os.Stdout, "  sub rax, %v\n", expectNumber())
+			continue
+		}
+
+		panic(fmt.Errorf("Unexpected character: '%s'", token))
 	}
 
 	fmt.Fprintf(os.Stdout, "  ret\n")
